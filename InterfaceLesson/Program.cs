9a100f9201c8781634a3e58ce8076e3bbda0ca0b
John Atkins <jdatkins@tql.com>
2021-05-25 17:22:52 -0400

    Add project files.

---
+++ InterfaceLesson/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace InterfaceLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pb = new PitBull();
+            var ch = new Chihuahua();
+            var doge = new ShibaInu();
+
+            var dogs = new IPrint[] { pb, ch, doge };
+
+            foreach (var dog in dogs)
+            {
+                dog.Print();
+            }
+        }
+    }
+}
